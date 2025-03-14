33ea833a9e532a8ed3364168670010120e57a3ad
FlamingHerb <mbomisol@up.edu.ph>
2025-03-14 13:18:53 +0800

    chore: added specific parts to main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+
+	// This can be false if you're using a fallback (i.e. SPA mode)
+	export const prerender = true;
 </script>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Hero from './Hero.svelte';
+    import Navbar from './Navbar.svelte';
+    import Overview from './Overview.svelte';
+    import Problem from './Problem.svelte';
+    import Data from './Data.svelte';
+    import Preprocessing from './Preprocessing.svelte';
+    import Results from './Results.svelte';
+</script>
+
+<Navbar />
+
+<!-- Introduction to the entire thing -->
+<Hero />
+
+<!-- Overview -->
+<Overview />
+
+<!-- Problem, Solution, Objectives & Hypothesis -->
+<Problem />
+
+<!-- Data History and Collection -->
+<Data />
+
+<!-- Data Preprocessing -->
+<Preprocessing />
+
+<!-- Results -->
+<Results />

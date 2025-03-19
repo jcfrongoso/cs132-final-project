9990f93b72f8c8366896eaf7826ad9e5e15d67b9
FlamingHerb <mbomisol@up.edu.ph>
2025-03-19 17:14:39 +0800

    chore: one more

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {adapter: adapter({
-	  pages: 'build',
-	  assets: 'build',
 	  fallback: '404.html'
 	}) },
 	// paths: {

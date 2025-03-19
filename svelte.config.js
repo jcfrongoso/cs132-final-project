b1d596d9435869aae6a6710d31b9d541898ab70f
jcfrongoso <i_justinclyde.frongoso@stratpoint.com>
2025-03-19 19:54:44 +0800

    feat: update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,10 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true,
 			fallback: '404.html'
 		}),
 		paths: {

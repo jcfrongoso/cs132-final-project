06889900857701483738fb2c41c720a5ba8ec2a0
jcfrongoso <i_justinclyde.frongoso@stratpoint.com>
2025-03-20 21:04:18 +0800

    feat: fix data.svelte content

--- src/routes/Data.svelte
+++ src/routes/Data.svelte
@@ -22,17 +22,24 @@
     <h2 class="mb-2">Data Preprocessing</h2>
     <ul class="list-outside list-disc">
         <li>
-            The first step done for data preprocessing is by checking the structure of the datasets. As shown from the notebook below, both datasets have the same structure: specie, geolocation, and the quarterly values/volumes for each specie/geolocation grouping.
+            The first step done was to check the structure of the datasets. On our Colab notebook, we were able to identify that both datasets have the same structure: species, geolocation, and the quarterly values/volumes for each species/geolocation grouping.
         </li>
         <li>
-            After identifying the structure of each dataset, we proceed to check whether we need to standardize colum values or impute for missing values. Since there are no missing values on any column, we proceed to check if we can standardize any of the values. Based on the values of the `Species` column, we can remove leading periods. The same goes to the `Geolocation` column.
+            After identifying the structure of each dataset, we proceed to check for missing values and apply imputation. Since both datasets have missing values, we proceed to apply KNN imputation. This method of imputation was used because standard imputation methods may lead to reduced variance and may cause to biased results/analysis. KNN imputation utilizes other features to identify what value to use instead of relying on one column to infer what value to impute that's why it was chosen as the method for imputation. 
         </li>
         <li>
-            After standardizing the columns, we transform the wide formatting into a long formatting using `melt()` in order to properly structure the datasets into time-series format.
+            Next step is to identify what to do with outliers. With regards to this problem, our group didn't check nor remove outlier values (value/volume that is past 3 standard deviations) since these values may have been caused by outside interactions/factors. We also took note of the source for these datasets because the sampling and collection weren't done by our group but rather by professional individuals which means that the values provided on these datasets are reliable and consistent.
         </li>
         <li>
-            We then combine the two datasets to prepare the final dataset that will be analyzed. The finalized dataset can be found <a target="_blank" rel="noreferrer" class="font-medium text-blue-600 hover:underline" href="https://docs.google.com/spreadsheets/d/1Clv7PSyVfpd0Z6B8E9nZ_KqdCiAqs-3NHwGAkvegNkY/edit?usp=sharing">here</a>.
+            Since we've skipped the outlier part, our group proceeded to reformat the columns and the dataset itself. Based on the values of the `Species` column, we can remove leading periods. The same goes to the `Geolocation` column. After reformatting the `Species` and `Geolocation` columns, we transform the wide formatted dataset into long formatting using `melt()` in order to properly structure the datasets into time-series format.
+        </li>
+        <li>
+            After applying all these steps on both datasets, our group finally combined them to prepare the final dataset that will be analyzed. The finalized dataset can be found <a target="_blank" rel="noreferrer" class="font-medium text-blue-600 hover:underline" href="https://docs.google.com/spreadsheets/d/1GGl72rv4w5Nj0xWuO4UrPLCey6OTtPAnVTV8sImYsqU/edit?usp=sharing">here</a>.
         </li>
     </ul>
-    <iframe title="Data Processing" src="{base}/data-preprocessing-proper.html" width="100%" height="600px"></iframe>
+    <a href="https://colab.research.google.com/drive/1oohGaBcZXjV41udiQipQl1Lw0ZCKSvzl?usp=sharing" target="_blank">
+        <button style="margin-top: 10px; padding: 10px 15px; background-color: #4285F4; color: white; border: none; border-radius: 5px; cursor: pointer;">
+            Open in Google Colab
+        </button>
+    </a>
 </div>

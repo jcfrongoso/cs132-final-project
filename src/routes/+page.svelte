e250e12106acf55a3e91099e1d1f9afd9800da96
FlamingHerb <mbomisol@up.edu.ph>
2025-03-20 18:29:08 +0800

    style: added <hr> between sections to give more clarity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,20 +15,28 @@
     <Hero />
 </section>
 
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
+
 <!-- Overview -->
 <section class="my-8 mx-8 sm:mx-auto" id="Overview">
     <Overview />
 </section>
+
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 
 <!-- Problem, Solution, Objectives & Hypothesis -->
 <section class="my-8 mx-8 sm:mx-auto" id="Problem">
     <Problem />
 </section>
 
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
+
 <!-- Data History and Collection -->
 <section class="my-8 mx-8 sm:mx-auto" id="Data">
     <Data />
 </section>
+
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 
 {#if render_results}
 <!-- Data Preprocessing -->
@@ -36,10 +44,14 @@
     <Preprocessing />
 </section>
 
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
+
 <!-- Results -->
 <section class="my-8 mx-8 sm:mx-auto">
     <Results />
-</section>    
+</section>
+
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 {/if}
 
 <!-- Team -->

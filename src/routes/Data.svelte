c391919766170a71f9171cb33bd57b5c505fdc4c
jcfrongoso <jvfrongoso@up.edu.ph>
2025-03-21 17:55:44 +0800

    feat: update Data.svelte contents

--- src/routes/Data.svelte
+++ src/routes/Data.svelte
@@ -19,24 +19,25 @@
     </p>
     <h3 class="mb-2">About the Dataset</h3>
     <p class="mb-4">
-        For this project, two datasets were used: <a class="font-medium text-blue-600 hover:underline" href="{aquaculture_value_link}" target="_blank" rel="noreferrer">quarterly aquaculture value</a> and <a class="font-medium text-blue-600 hover:underline" href="{aquaculture_volume_link}">quarterly aquaculture volume</a> for each region, scoping the first quarter of 2020 to the fourth quarter of 2024. The first dataset contains the value of each aquaculture specie for each region while the second dataset contains the volume of each aquaculture specie for each region.
+        For this project, two datasets were used: <a class="font-medium text-blue-600 hover:underline" href="{aquaculture_value_link}" target="_blank" rel="noreferrer">quarterly aquaculture value</a> and <a class="font-medium text-blue-600 hover:underline" href="{aquaculture_volume_link}">quarterly aquaculture volume</a> for each region, scoping the first quarter of 2020 to the fourth quarter of 2024. The first dataset contains the value of each aquaculture specie for each region while the second dataset contains the volume of each aquaculture specie for each region. The finalized dataset that will be used for analysis contains six columns, namely Species, Geolocation, Year, Quarter, Value, and Volume, and 6800 samples.
     </p>
     <h2 class="mb-2">Data Preprocessing</h2>
+    <p class="mb-4">
+        For data preporcessing, four major steps were used on both dataset to arrive on the combined final dataset. Before proceeding to the four major steps, a preliminary check was done on both datasets to gain a brief understanding about the structure and format of both datasets.
+    </p>
     <ul class="list-outside list-disc">
         <li>
-            The first step done was to check the structure of the datasets. On our Colab notebook, we were able to identify that both datasets have the same structure: species, geolocation, and the quarterly values/volumes for each species/geolocation grouping.
+            The first step done was to check the structure of both datasets. On our Colab notebook, we were able to identify that both datasets have the same structure: both contains species, geolocation, and the quarterly values/volumes for each species/geolocation grouping and a wide formatting due to the nature on how quarterly value/volumes were structured.
         </li>
         <li>
-            After identifying the structure of each dataset, we proceed to check for missing values and apply imputation. Since both datasets have missing values, we proceed to apply KNN imputation. This method of imputation was used because standard imputation methods may lead to reduced variance and may cause to biased results/analysis. KNN imputation utilizes other features to identify what value to use instead of relying on one column to infer what value to impute that's why it was chosen as the method for imputation. 
+            The second step was to identify missing values and apply imputation, if necessary. It was found out that both datasets contain missing values in the form of 
+            ".." strings. From here, we preprocess those strings in such a way that we could apply KNN imputation to fill up the gaps. This method of imputation was used because standard imputation methods, such as mean, mode, median, backward, and forward fill, may lead to reduced variance and a possibility of data leakage. KNN imputation utilizes other features to identify what value to use instead of relying on one column to infer what value to impute that's why it was chosen as the method for imputation.
         </li>
         <li>
-            Next step is to identify what to do with outliers. With regards to this problem, our group didn't check nor remove outlier values (value/volume that is past 3 standard deviations) since these values may have been caused by outside interactions/factors. We also took note of the source for these datasets because the sampling and collection weren't done by our group but rather by professional individuals which means that the values provided on these datasets are reliable and consistent.
+            The third step done was to reformat the columns and the dataset themselves. We've skipped the outlier part since these values may have been caused by outside interactions/factors. We also took note of the source for these datasets because the sampling and collection weren't done by our group but rather by professional individuals which means that the values provided on these datasets are reliable and consistent. Based on the values of the Species column, we can remove leading periods. The same goes with the Geolocation column. After reformatting both columns, we transform the wide formatted dataset into long formatting using melt() in order to properly structure the datasets into a format suitable for time-series analysis.
         </li>
         <li>
-            Since we've skipped the outlier part, our group proceeded to reformat the columns and the dataset itself. Based on the values of the `Species` column, we can remove leading periods. The same goes to the `Geolocation` column. After reformatting the `Species` and `Geolocation` columns, we transform the wide formatted dataset into long formatting using `melt()` in order to properly structure the datasets into time-series format.
-        </li>
-        <li>
-            After applying all these steps on both datasets, our group finally combined them to prepare the final dataset that will be analyzed. The finalized dataset can be found <a target="_blank" rel="noreferrer" class="font-medium text-blue-600 hover:underline" href="{finalized_dataset}">here</a>.
+            The fourth step was to combine both datasets into a finalized dataset that will be used for the analaysis step. The finalized dataset can be found <a target="_blank" rel="noreferrer" class="font-medium text-blue-600 hover:underline" href="{finalized_dataset}">here</a>.
         </li>
     </ul>
     <a href="{colab_link}" target="_blank">

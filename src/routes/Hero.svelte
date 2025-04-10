702ed15267fbb517993892dc66946f537161d4ab
jcfrongoso <i_justinclyde.frongoso@stratpoint.com>
2025-04-10 12:39:19 +0800

    feat: add init eda

--- src/routes/Hero.svelte
+++
@@ -1,3 +0,0 @@
-<div class="max-w-5xl mx-auto">
-    <h1 class="text-left">Aquaculture Project</h1>
-</div>
--- src/routes/Preprocessing.svelte
+++
@@ -1,5 +0,0 @@
-
-<div class="max-w-5xl mx-auto">
-    <h1>Preprocessing</h1>
-    <h2>Modeling</h2>
-</div>

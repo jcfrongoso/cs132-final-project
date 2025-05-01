d9be5f46ad1a87c3a06d85b02d1598373ecf3bfa
FlamingHerb <mbomisol@up.edu.ph>
2025-05-01 21:44:32 +0800

    chore: adding in placeholders at Preprocessing.svelte

--- src/routes/Preprocessing.svelte
+++ src/routes/Preprocessing.svelte
@@ -1,8 +1,31 @@
 <script>
+    import { base } from '$app/paths';
+
+    // Place images here
+    // Univariate images.
+    import uni_species from '$lib/images/teehee.png';
+    import uni_geolocation from '$lib/images/teehee.png';
+    import uni_volume from '$lib/images/teehee.png';
+    import uni_value from '$lib/images/teehee.png';
+    
+    // Bivariate images.
+    import bi_species_value from '$lib/images/teehee.png';
+    import bi_species_volume from '$lib/images/teehee.png';
+    import bi_geolocation_value from '$lib/images/teehee.png';
+    import bi_geolocation_volume from '$lib/images/teehee.png';
+    import bi_value_yearquarter from '$lib/images/teehee.png';
+    import bi_volume_yearquarter from '$lib/images/teehee.png';
+    import bi_value_volume from '$lib/images/teehee.png';
+
+    // Mutlivariate images.
+    import multi_value_species from '$lib/images/teehee.png';
+    import multi_value_region from '$lib/images/teehee.png';
+    import multi_volume_species from '$lib/images/teehee.png';
+    import multi_volume_region from '$lib/images/teehee.png';
+
+
     let finalized_dataset = "https://docs.google.com/spreadsheets/d/1GGl72rv4w5Nj0xWuO4UrPLCey6OTtPAnVTV8sImYsqU/edit?usp=sharing";
     let colab_link = "https://colab.research.google.com/drive/1oohGaBcZXjV41udiQipQl1Lw0ZCKSvzl?usp=sharing";
-
-    import { base } from '$app/paths';
 </script>
 <div class="max-w-5xl mx-auto">
     <h1 class="mb-2">Data Preprocessing</h1>
@@ -33,6 +56,88 @@
         </a>
     </div>
     <h2 class="mb-2">Data Visualization</h2>
+    
+    <!-- Univariate Analysis -->
+
+    <h2 class="mb-2">Univariate Analysis</h2>
+    <p class="mb-4"></p>
+    
+
+    <h3 class="mb-2">Species</h3>
+    <img alt="Placeholder" class="w-1/6" src={uni_species} />
+
+    <p class="mb-4"></p>
+    <h3 class="mb-2">Geolocation</h3>
+    <img alt="Placeholder" class="w-1/6" src={uni_geolocation} />
+    <p class="mb-4"></p>
+
+    <h3 class="mb-2">Volume</h3>
+    <img alt="Placeholder" class="w-1/6" src={uni_volume} />
+    <p class="mb-4"></p>
+    
+    <h3 class="mb-2">Value</h3>
+    <img alt="Placeholder" class="w-1/6" src={uni_value} />
+    <p class="mb-4"></p>
+    
+    <!-- Bivariate Analysis -->
+    
+    <h2 class="mb-2">Bivariate Analysis</h2>
+    <p class="mb-4"></p>
+    
+
+    <h3 class="mb-2">Species - Value</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_species_value} />
+    <p class="mb-4"></p>
+
+    <h3 class="mb-2">Species - Volume</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_species_volume} />
+    <p class="mb-4"></p>
+
+    <h3 class="mb-2">Geolocation - Value</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_geolocation_value} />
+    <p class="mb-4"></p>
+
+    <h3 class="mb-2">Geolocation - Volume</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_geolocation_volume} />
+    <p class="mb-4"></p>
+
+    <h3 class="mb-2">Value - Year/Quarter</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_value_yearquarter} />
+    <p class="mb-4"></p>
+    
+    <h3 class="mb-2">Volume - Year/Quarter</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_volume_yearquarter} />
+    <p class="mb-4"></p>
+    
+    <h3 class="mb-2">Value - Volume</h3>
+    <img alt="Placeholder" class="w-1/6" src={bi_value_volume} />
+    <p class="mb-4"></p>
+    
+    
+    <!-- Multivariate Analysis -->
+    
+    <h2 class="mb-2">Multivariate Analysis</h2>
+    <p class="mb-4"></p>
+
+
+    <h3 class="mb-2">Value - Year/Quarter per Species</h3>
+    <img alt="Placeholder" class="w-1/6" src={multi_value_species} />
+    <p class="mb-4"></p>
+    
+    <h3 class="mb-2">Value - Year/Quarter per Region</h3>
+    <img alt="Placeholder" class="w-1/6" src={multi_value_region} />
+    <p class="mb-4"></p>
+    
+    <h3 class="mb-2">Volume - Year/Quarter per Species</h3>
+    <img alt="Placeholder" class="w-1/6" src={multi_volume_species} />
+    <p class="mb-4"></p>
+    
+    <h3 class="mb-2">Volume - Year/Quarter per Region</h3>
+    <img alt="Placeholder" class="w-1/6" src={multi_volume_region} />
+    <p class="mb-4"></p>
+    
+
+
     <h2 class="mb-2">Data Modeling</h2>
     <h2 class="mb-2">Nutshell Graph</h2>
 </div>

2bab23708af28b7ac37ce15889fd79caa647dfa5
jcfrongoso <173990521+jcfrongoso@users.noreply.github.com>
2025-05-05 02:29:40 +0800

    feat: add nutshell plot and rq1 content

--- src/routes/Results.svelte
+++ src/routes/Results.svelte
@@ -2,26 +2,10 @@
     import { base } from '$app/paths';
 
     // Place images here
-    // Univariate images.
-    import uni_species from '$lib/images/teehee.png';
-    import uni_geolocation from '$lib/images/teehee.png';
-    import uni_volume from '$lib/images/teehee.png';
-    import uni_value from '$lib/images/teehee.png';
-
-    // Bivariate images.
-    import bi_species_value from '$lib/images/teehee.png';
-    import bi_species_volume from '$lib/images/teehee.png';
-    import bi_geolocation_value from '$lib/images/teehee.png';
-    import bi_geolocation_volume from '$lib/images/teehee.png';
-    import bi_value_yearquarter from '$lib/images/teehee.png';
-    import bi_volume_yearquarter from '$lib/images/teehee.png';
-    import bi_value_volume from '$lib/images/teehee.png';
-
-    // Mutlivariate images.
-    import multi_value_species from '$lib/images/teehee.png';
-    import multi_value_region from '$lib/images/teehee.png';
-    import multi_volume_species from '$lib/images/teehee.png';
-    import multi_volume_region from '$lib/images/teehee.png';
+    import rq1_1 from '$lib/images/plots/4-1 Relationship between Production Volume and Economic Value.png'
+    import rq1_2 from '$lib/images/plots/4-1 KMeans Clustering.png'
+    import rq1_3 from '$lib/images/plots/4-1 Kmeans with regression lines.png'
+    import nutshell_plot from '$lib/images/plots/5 Nutshell Plot.png' ;
 </script>
 
 <div class="max-w-5xl mx-auto">
@@ -37,10 +21,16 @@
         <h3  class="mb-2">
             `Ha`: There is a significant correlation between aquaculture volume and value.
         </h3>
-        <p  class="mb-2">Our first research question focuses on the significance of these two values in determining our support for aquaculture systems in the Philippines. With proper knowledge and analysis of these features, we get to understand the position of aquaculture systems in our country. We can use this knowledge to identify what key points lead to the increase/decrease of these systems in order to maintain a stable economy.</p>
-        <p  class="mb-2">For the first research question, we are dealing with a quantitative independent variable Volume and a quantitative dependent variable Value. Given this, we opted to use linear regression to identify the correlation between the two variables. Using StatsModels API on Ordinary Least Squares, we were able to identify the following results.</p>
-        <p  class="mb-2">insert results here</p>
-        <p  class="mb-2">Given the summary results provided from our OLS regression, we conclude that we should reject the null hypothesis with $P > |t|$, t-statistic value of 37.296 which is large, and with the 95% confidence interval [0.015, 0.016] not including zero which means that there is a positive correlation between the two.</p>
+        <p  class="mb-2">The first research question deals with two quantitative values: volume and value. Since we are testing if there is no positive correlation between the two features and not just any relation, we proceed to use one-tailed Pearson correlation test.</p>
+        <p  class="mb-2">Based on our Pearson correlation coefficient, there is an indication that there's a moderate linear positive relationship between production volume and economic value. This means that as aquaculture production volume increases, the economic value tends to increase as well. The one-tailed p-value of 0.0000 means that the probability of observing this correlation by random chance is virtually zero. Since p-value is less than 0.05, we reject the null hypothesis.</p>
+        <p class="mb-2">To visually support this claim, we can utilize our plot from 3.2.7 but now we add a regression line.</p>
+        <img alt="Placeholder" class="w-xl" src={rq1_1} />
+        <h2>IMPLICATIONS</h2>
+        <p  class="mb-2">From our earlier discussion regarding the scatter plot, we brought up that there are two visual clusterings that can be made that's why the current regression line identified from the current plot is questionnable in some sense. So, if there are indeed two possible clustering, what could it be? Here, we use K-means clustering to identify the two visually identifiable clustering.</p>
+        <img alt="Placeholder" class="w-xl" src={rq1_2} />
+        <p class="mb-2">The plot above is quite interesting as compared to what we could have imagined (most would easily halve the original scatter plot into two by creating a dividing line that will cluster the original scatter plot). But does these two plots inherit the same regression line from the original plot?</p>
+        <img alt="Placeholder" class="w-xl" src={rq1_3} />
+        <p>The first clustering has a high positive coefficient while the second clustering tends to have a lower negative coefficient. Given these two plots, it is hard to conclude what characteristics can be discerned from approximately 66% of production volume values. But after some point, economic value tends to die down. This might make sense due to the law of demand and supply. After some point, we cannot value the same good similar to the one previously bought due to decreasing marginal utility. Aside from that, there are income effects and demand shifts that needs to be considered especially that we are living in a third-world country and are currently facing high taxes. With this, we can observe that higher production volumes tend to be associated with higher economic values. Do note that we are only speaking in an independent manner where we don't take into account various other factors that may lead to sudden shifts in economic value or production volume. This is still worth looking into a higher perspective.</p>
     </div>
     <div>
         <h2  class="mb-2">Research Question 2: Is there a significant difference in aquaculture volume across different regions in the Ph?</h2>
@@ -66,8 +56,10 @@
         <p  class="mb-2">Given the H value of 37.7 which is quite large and a p value of $1.31\times 10^{-7}$, we also reject the null hypothesis. Thus, there is a significant difference in aquaculture volume over the years.</p>
         WHAAT
     </div>
-
-    <h3 class="mb-2">Data Modeling</h3>
     
-    <h3 class="mb-2">Nutshell Graph</h3>
+    <h3 class="mb-2">What's our take?</h3>
+    <img alt="Placeholder" class="w-5xl" src={nutshell_plot} />
+    <p>
+        With these things in mind, we get to see a bigger picture. Having resources is not enough for an economy to work without proper support. Even though a large portion of Visayas is surrounded by a large body of water, they still lack in terms of production volume. But is it the end-all? No, there are prospering regions that dare defy this case. Central Luzon is one of the leading regions in terms of production volume but in their case, they are not as geographically resourceful in terms of bodies of water. This might mean one thing. Other regions are not trying. ...
+    </p>
 </div>

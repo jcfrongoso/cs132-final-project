32b0a948830c502c058b254fce03118e647f4d03
FlamingHerb <mbomisol@up.edu.ph>
2025-05-05 14:59:37 +0800

    chore: adding project title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,28 +11,28 @@
 </script>
 
 <!-- Introduction to the entire thing -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Hero">
+<section class="my-8 mx-8 sm:mx-auto" id="Hero">
     <Hero />
 </section>
 
 <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 
 <!-- Overview -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Overview">
+<section class="my-8 mx-8 sm:mx-auto" id="Overview">
     <Overview />
 </section>
 
 <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 
 <!-- Problem, Solution, Objectives & Hypothesis -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Problem">
+<section class="my-8 mx-8 sm:mx-auto" id="Problem">
     <Problem />
 </section>
 
 <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 
 <!-- Data History and Collection -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Data">
+<section class="my-8 mx-8 sm:mx-auto" id="Data">
     <Data />
 </section>
 
@@ -40,7 +40,7 @@
 
 {#if render_results}
 <!-- Data Preprocessing -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Preprocessing">
+<section class="my-8 mx-8 sm:mx-auto" id="Preprocessing">
     <Preprocessing />
     <!-- <Preprocessing /> -->
 </section>
@@ -48,7 +48,7 @@
 <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-300">
 
 <!-- Results -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Results">
+<section class="my-8 mx-8 sm:mx-auto" id="Results">
     <Results />
 </section>
 
@@ -56,6 +56,6 @@
 {/if}
 
 <!-- Team -->
-<section class="my-8 pt-20 mx-8 sm:mx-auto" id="Team">
+<section class="my-8 mx-8 sm:mx-auto" id="Team">
     <Team />
 </section>
--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,3 +1,4 @@
 <div class="max-w-5xl mx-auto">
-    <h1 class="text-left">Aquaculture Project</h1>
+    <h1 class="text-left">Floating Ashore</h1>
+    <h2>A Dive in the State of Philippine Aquaculture Systems</h2>
 </div>

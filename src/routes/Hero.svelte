8b98e1937a63007d1780cbe814eada4467412c8d
jcfrongoso <173990521+jcfrongoso@users.noreply.github.com>
2025-05-05 17:23:29 +0800

    feat: update hero.svelte title

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,4 +1,4 @@
 <div class="max-w-5xl mx-auto">
-    <h1 class="text-left">Floating Ashore</h1>
-    <h2>A Dive in the State of Philippine Aquaculture Systems</h2>
+    <h1 class="text-left">Beneath the Surface</h1>
+    <h2>Regional Disparities in Aquaculture Production and Volume</h2>
 </div>

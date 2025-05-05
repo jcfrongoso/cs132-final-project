5acbaf695418d0d8da0b67532db8a88126e98de5
jcfrongoso <173990521+jcfrongoso@users.noreply.github.com>
2025-05-05 11:01:34 +0800

    feat: update nutshell plot content

--- src/routes/Results.svelte
+++ src/routes/Results.svelte
@@ -82,6 +82,9 @@
     <h3 class="mb-2">What's our take?</h3>
     <img alt="Placeholder" class="w-5xl" src="{nutshell_plot}" />
     <p>
-        With these things in mind, we get to see a bigger picture. Having resources is not enough for an economy to work without proper support. Even though a large portion of Visayas is surrounded by a large body of water, they still lack in terms of production volume. But is it the end-all? No, there are prospering regions that dare defy this case. Central Luzon is one of the leading regions in terms of production volume but in their case, they are not as geographically resourceful in terms of bodies of water. This might mean one thing. Other regions are not trying. ...
+       With these things in mind, we get to see a bigger picture. Having resources is not enough for an economy to work without proper support. Even though a large portion of Visayas is surrounded by a large body of water, they still lack in terms of production volume. But is it the end-all? No, there are prospering regions that dare defy this case. Central Luzon is one of the leading regions in terms of production volume but in their case, they are not as geographically resourceful in terms of bodies of water. This might mean one thing. Other regions are not trying. 
+    </p>
+    <p>
+        Or it could be that the disparity lies in access to aquaculture infrastructure, policy support, and investment in technology. BARMM, for instance, dominates production—suggesting that when regions are empowered, productivity can skyrocket regardless of long-standing challenges. Meanwhile, the map also reveals that economic value doesn’t always correlate directly with production volume, hinting at deeper systemic issues in pricing, value chains, and market access. This further emphasizes the need for a more equitable and strategic approach to aquaculture development across regions.
     </p>
 </div>

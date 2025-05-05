d59e20ea90023343f9e13f6dc60b886abe305518
FlamingHerb <mbomisol@up.edu.ph>
2025-05-05 19:44:02 +0800

    config: force vite to include everything regardless

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	base: 'https://jcfrongoso.github.io/cs132-final-project/',
+	assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg']
 });

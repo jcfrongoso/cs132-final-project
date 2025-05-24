a34094924b5527bbd3fafb35864156ddcb52a37d
FlamingHerb <mbomisol@up.edu.ph>
2025-05-25 02:00:40 +0800

    style: hero and bg gradient

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -9,7 +9,11 @@
       </div>
     <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
         <img class="h-[150px] mb-4" src="{base}/favicon.png">
-        <h1 class="leading-tight mb-4 text-6xl">Beneath the Surface</h1>
+        <h1 
+            class="leading-tight mb-4 text-6xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text"
+        >
+            Beneath the Surface
+        </h1>
         <p class="text-lg mb-8">Regional Disparities in Aquaculture Production and Volume</p>
         <p class="text-sm text-gray-300 w-[720px] mb-4 text-center">
         Our project, "Aquaculture in the Philippines", utilizes data science techniques to analyze economic trends, assess regional variations, and evaluate the investment potential of the country's aquaculture industry. By examining the growth and distribution of various marine species across different regions, we aim to provide data-driven insights into the viability of further investment in Philippine aquaculture.

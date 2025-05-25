1963d23920bf9fa9485a622c66e618b6d21de594
FlamingHerb <mbomisol@up.edu.ph>
2025-05-25 11:23:13 +0800

    style: overhaul Nutshell.svelte

--- src/routes/Nutshell.svelte
+++ src/routes/Nutshell.svelte
@@ -2,13 +2,19 @@
     import { base } from '$app/paths';
     import nutshell_plot from '$lib/images/plots/5_Nutshell_plot.png';
 </script>
-<div class="max-w-5xl mx-auto bg-gray-200 p-6 pt-10 rounded-lg">
-    <h1 class="mb-4">What's our take? (Nutshell Plot)</h1>
-    <p class="mb-4">
-       With these things in mind, we get to see a bigger picture. Having resources is not enough for an economy to work without proper support. Even though a large portion of Visayas is surrounded by a large body of water, they still lack in terms of production volume. But is it the end-all? No, there are prospering regions that dare defy this case. Central Luzon is one of the leading regions in terms of production volume but in their case, they are not as geographically resourceful in terms of bodies of water. This might mean one thing. Other regions are not trying. 
-    </p>
-    <p class="mb-4">
-        Or it could be that the disparity lies in access to aquaculture infrastructure, policy support, and investment in technology. BARMM, for instance, dominates production—suggesting that when regions are empowered, productivity can skyrocket regardless of long-standing challenges. Meanwhile, the map also reveals that economic value doesn’t always correlate directly with production volume, hinting at deeper systemic issues in pricing, value chains, and market access. This further emphasizes the need for a more equitable and strategic approach to aquaculture development across regions.
-    </p>
-    <img alt="Placeholder" class="w-5xl my-4 mx-auto" src="{base}/images/plots/5_Nutshell_plot.png" />
+<div class="max-w-5xl mx-auto bg-[#161a1a] pb-6 pr-6 pt-10 rounded-lg">
+    <h1 class="pl-6 mb-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">What's our take? In a nutshell...</h1>
+    <div class="grid grid-cols-[65%_35%]">
+        <div class="flex items-center justify-center">
+            <img alt="Placeholder" class="w-full mx-auto" src="{base}/images/plots/5_Nutshell_plot.png" />
+        </div>
+        <div class="text-right text-[13px] text-gray-300 pt-4 pl-6">
+            <p class="mb-4">
+                With these things in mind, we get to see a bigger picture. Having resources is not enough for an economy to work without proper support. Even though a large portion of Visayas is surrounded by a large body of water, they still lack in terms of production volume. But is it the end-all? No, there are prospering regions that dare defy this case. Central Luzon is one of the leading regions in terms of production volume but in their case, they are not as geographically resourceful in terms of bodies of water. This might mean one thing. Other regions are not trying. 
+            </p>
+            <p class="mb-4">
+                Or it could be that the disparity lies in access to aquaculture infrastructure, policy support, and investment in technology. BARMM, for instance, dominates production—suggesting that when regions are empowered, productivity can skyrocket regardless of long-standing challenges. Meanwhile, the map also reveals that economic value doesn’t always correlate directly with production volume, hinting at deeper systemic issues in pricing, value chains, and market access. This further emphasizes the need for a more equitable and strategic approach to aquaculture development across regions.
+            </p>
+        </div>
+    </div>
 </div>

7da7b73fd55f03774850878743e64a2baf32e4e3
smurfedGitHubIO <13251277+smurfedGitHubIO@users.noreply.github.com>
2025-05-27 21:09:25 +0800

    feat: add content for conclusion

--- src/routes/Conclusion.svelte
+++ src/routes/Conclusion.svelte
@@ -1,5 +1,14 @@
 <div class="max-w-5xl mx-auto bg-gray-200 p-6 pt-10 rounded-lg">
     <h1 class="mb-4">Conclusion</h1>
+    <p>
+        In this study, we were able to learn about aquaculture systems in the Philippines. By applying statistical analyses on regional disparities, we were able to identify a moderate positive correlation between production volume and economic value which means that an increase in production can contribute to to economic returns. However, there are no clear signs regarding production trend over time which may lead to the sector's complexities and challenges.
+    </p>
     <h2 class="mb-4">Call to Action</h2>
+    <p>
+        These challenges provide opportunities for policymakers, researchers, and industry stakeholders to collaborate and provide strategies to solve the root cause of these problems. Investment on underperforming regions, enhanced support systems, and active monitoring on production data are just some of the efforts that may be done based on our report. Integrating additional factors aside from the ones considered for this study may help ensure both economic viability and food security.
+    </p>
     <h2 class="mb-4">Recommendation</h2>
-</div> 
+    <p>
+        From here, the team recommends to identify additional factors in understanding the correlation between the variables. Also, when dealing with time series data, it is highly recommended to use fine-grained data in order to better analyze and identify trends since relevant information is better understood when studied under finer details.
+    </p>
+</div>
